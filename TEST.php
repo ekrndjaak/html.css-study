d03f8ae3ef3461fd4eba12565f5918286eb697d6
dlagksrb_00 <dlagksrb0430@naver.com>
2023-06-20 16:41:11 +0900

    0620 change

---
+++ TEST.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HTML과 PHP 연결</title>
+</head>
+<body>
+    <h1>HTML과 PHP 연결 예시</h1>
+
+    <?php
+    // PHP 코드 작성 부분
+    $name = "John Doe";
+    echo "안녕하세요, " . $name . "님!";
+    ?>
+
+</body>
+</html>
